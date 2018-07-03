dece002ad9477661011478fa6bbf4b60e4bd3301
Harjinder <brar5353@yahoo.com>
2018-07-03 14:42:57 +1000

    postgres cookbook

---
+++ README.md
@@ -0,0 +1,4 @@
+# postgres
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook:: postgres
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+#following the instructions on https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-centos-7
+
+package %w(postgresql-server postgresql-contrib) do 
+  action :install
+end
+
+#initialize the cluster
+execute 'initialize-the-cluster' do
+    command 'postgresql-setup initdb'
+end
+
+#change password for postgres user
+#sudo -u postgres psql postgres
+
+
+#update pg_hba.conf from template, restart pgsql when template is loaded
+template '/var/lib/pgsql/data/pg_hba.conf' do
+  action :create
+  source 'pg_hba.conf.erb'
+  notifies :restart, 'service[postgresql]'
+end
+
+#start and enable service on startup
+service "postgresql" do
+  action [ :enable, :start ]
+end 
+
+#add postgresql.conf template
+template '/var/lib/pgsql/data/postgresql.conf' do
+  action :create
+  source 'postgresql.conf.erb'
+  notifies :restart, 'service[postgresql]'
+end
+
+
+#add attributes
+#add databags
